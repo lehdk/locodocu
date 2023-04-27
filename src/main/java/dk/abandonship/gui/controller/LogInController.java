b2a4a11a1f8c381a9ba2942bd8627259fdf5fb4a
TheDarkSide1992 <55052002+TheDarkSide1992@users.noreply.github.com>
2023-04-27 12:19:34 +0200

    Log in view (#4)
    * creted GUI FXML
    
    * Stylling and css file
    
    * Update LogInController.java

--- src/main/java/dk/abandonship/gui/controller/LogInController.java
+++ src/main/java/dk/abandonship/gui/controller/LogInController.java
@@ -1,13 +1,28 @@
 package dk.abandonship.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LogInController implements Initializable {
+    @FXML private PasswordField fieldPassword;
+    @FXML private TextField fieldUserName;
+    @FXML private Button btnLogIn;
+    @FXML private Label lblError;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void handleLogIn() {
+        lblError.setText("Invalid username or password");
     }
 }
