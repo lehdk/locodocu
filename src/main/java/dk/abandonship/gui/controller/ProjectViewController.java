b56d81300edbb9bd621448909c1978bcd4a79c64
TheDarkSide1992 <Jenscp171@gmail.com>
2023-04-28 12:24:10 +0200

    content

--- src/main/java/dk/abandonship/gui/controller/ProjectViewController.java
+++ src/main/java/dk/abandonship/gui/controller/ProjectViewController.java
@@ -2,8 +2,12 @@ package dk.abandonship.gui.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -19,7 +23,31 @@ public class ProjectViewController implements Initializable {
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 
         for (int i = 0; i < 100; i++) {
-            vbox.getChildren().add(new Button("Reeee"));
+            Pane pane = new Pane();
+            VBox vBox = new VBox();
+            HBox hBox = new HBox();
+            Label label1 = new Label("lbl1");
+            label1.setPadding(new Insets(20,20,20,20));
+            hBox.getChildren().add(label1);
+
+            Label label2 = new Label("lbl2");
+            label2.setPadding(new Insets(20,20,20,20));
+            hBox.getChildren().add(label2);
+
+            Label label3 = new Label("lbl3");
+            label3.setPadding(new Insets(20,20,20,20));
+            hBox.getChildren().add(label3);
+
+            vBox.setAlignment(Pos.CENTER);
+            vBox.getChildren().add(hBox);
+            Button btn =new Button("reee");
+            vBox.getChildren().add(btn);
+
+            pane.setPrefSize(vBox.getPrefWidth()+60,vBox.getPrefHeight()+60);
+            pane.setStyle("-fx-background-color: #030202");
+            pane.getChildren().add(vBox);
+            pane.setPadding(new Insets(50,50,50,50));
+            vbox.getChildren().add(pane);
         }
     }
 }
