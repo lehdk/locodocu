f854c3aa74c27eacc139e8f7c59998b957c771bf
Lasse <leh99dk.mail@gmail.com>
2023-05-02 13:12:07 +0200

    Feature/add new customer (#18)
    * Can now add new customer.
    
    * Added test and comment.

--- src/main/java/dk/abandonship/dataaccess/interfaces/ICustomerDAO.java
+++ src/main/java/dk/abandonship/dataaccess/interfaces/ICustomerDAO.java
@@ -1,6 +1,7 @@
 package dk.abandonship.dataaccess.interfaces;
 
 import dk.abandonship.entities.Customer;
+import dk.abandonship.entities.CustomerDTO;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -8,9 +9,15 @@ import java.util.List;
 public interface ICustomerDAO {
 
     /**
-     * Gets all the customers from the datasource
+     * Gets all the customers from the datasource.
      * @return A list of customers. Empty list if no customers found.
      */
     List<Customer> getAllCustomers() throws SQLException;
 
+    /**
+     * Adds a customer to the data source.
+     * @param customerDTO The DTO of the customer you want to add.
+     * @return The newly added customer.
+     */
+    Customer addCustomer(CustomerDTO customerDTO) throws SQLException;
 }
--- src/main/java/dk/abandonship/gui/controller/CustomerController.java
+++ src/main/java/dk/abandonship/gui/controller/CustomerController.java
@@ -1,19 +1,30 @@
 package dk.abandonship.gui.controller;
 
+import dk.abandonship.Main;
 import dk.abandonship.entities.Customer;
+import dk.abandonship.entities.CustomerDTO;
+import dk.abandonship.gui.controller.PopUpController.AddCustomerController;
 import dk.abandonship.gui.model.CustomerModel;
 import dk.abandonship.state.LoggedInUserState;
 import dk.abandonship.utils.DefaultRoles;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class CustomerController implements Initializable {
@@ -53,6 +64,14 @@ public class CustomerController implements Initializable {
 
         if(LoggedInUserState.getInstance().hasRole(DefaultRoles.PROJECTMANAGER) || LoggedInUserState.getInstance().hasRole(DefaultRoles.SALESPERSON)) {
             Button addCustomerButton = new Button("Add Customer");
+            addCustomerButton.setOnAction(event -> {
+                try {
+                    handleAddCustomer();
+                } catch (IOException e) {
+                    System.err.println("Could not open window!");
+                    e.printStackTrace();
+                }
+            });
             buttonsHBox.getChildren().add(addCustomerButton);
 
             Button editCustomerButton = new Button("Edit Customer");
@@ -60,6 +79,32 @@ public class CustomerController implements Initializable {
 
             Button deleteCustomerButton = new Button("Delete Customer");
             buttonsHBox.getChildren().add(deleteCustomerButton);
+        }
+    }
+
+    public void handleAddCustomer() throws IOException {
+        Stage popupStage = new Stage();
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("gui/view/PopUps/AddCustomerView.fxml"));
+
+        Parent root = loader.load();
+        AddCustomerController controller = loader.getController();
+        Scene popupScene = new Scene(root);
+
+        popupStage.setScene(popupScene);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.initStyle(StageStyle.UNDECORATED);
+
+        popupStage.showAndWait();
+
+        CustomerDTO result = controller.getResult();
+        if(result == null) return;
+
+        try {
+            customerModel.addCustomer(result);
+        } catch (SQLException e) {
+            System.err.println("Could not add customer!");
+            e.printStackTrace();
         }
     }
 }
