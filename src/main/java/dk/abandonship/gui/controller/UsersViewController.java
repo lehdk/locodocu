2c305211c71332e8d23f68a2e63eb467580afbd3
Anders Madsen <114471768+andersmadsen0511@users.noreply.github.com>
2023-05-04 11:37:45 +0200

    Add user fxml added

--- src/main/java/dk/abandonship/gui/controller/UsersViewController.java
+++ src/main/java/dk/abandonship/gui/controller/UsersViewController.java
@@ -1,58 +1,37 @@
 package dk.abandonship.gui.controller;
 
 import dk.abandonship.Main;
-import dk.abandonship.entities.User;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.stage.Window;
 
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Timestamp;
 import java.util.ResourceBundle;
 
 public class UsersViewController implements Initializable {
 
     public Button openAddUserWindowButton;
-    @FXML
-    private TableView<User> userTable;
-    @FXML
-    private TableColumn<User, String> nameColumn;
-    @FXML
-    private TableColumn<User, String> emailColumn;
-    @FXML
-    private TableColumn<User, String> phoneColumn;
+    public VBox adminUsers;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
-        emailColumn.setCellValueFactory(new PropertyValueFactory<>("Email"));
-        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("Phone"));
 
-        ObservableList<User> users = FXCollections.observableArrayList(
-                new User(0, "Mærsk", "mærsk@mærsk.dk", "88888888", "1234", new Timestamp(1)),
-                new User(0, "Møller", "mærsk@møller.dk", "00000000", "1234", new Timestamp(1)));
-
-        userTable.setItems(users);
     }
-    
-    public void openNewUserWindow() throws IOException {
+
+    public void handleOpenNewUserWindow() throws IOException {
         Stage popupStage = new Stage();
 
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("presentation/view/NewUser.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/dk/abandonship/gui/view/NewUser.fxml"));
         Parent root = loader.load();
         Scene popupScene = new Scene(root);
 
@@ -61,5 +40,9 @@ public class UsersViewController implements Initializable {
         popupStage.initStyle(StageStyle.UNDECORATED);
 
         popupStage.showAndWait();
+    }
+
+    public void createUserNode() {
+
     }
 }
