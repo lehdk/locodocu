2ab38746a7e51f9877e35236eadbcaef020ef28f
Lehdk\Lehdk <leh99dk.mail@gmail.com>
2023-05-12 10:47:13 +0200

    No errors when creating a new documentation while already viewing one.

--- src/main/java/dk/abandonship/gui/controller/DocViewController.java
+++ src/main/java/dk/abandonship/gui/controller/DocViewController.java
@@ -136,6 +136,12 @@ public class DocViewController implements Initializable {
             controllerAssistant.displayError(e);
         }
 
+        displayExistingDocumentationNodes(documentation);
+    }
+
+    private void displayExistingDocumentationNodes(Documentation documentation) {
+        if(documentation == null) return;
+
         for (DocumentationNode dn: documentation.getDocumentationNodes()) {
             if (dn instanceof DocumentationTextFieldNode){
                 addTextFieldForEdit((DocumentationTextFieldNode) dn);
