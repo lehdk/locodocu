663f8d7176c3e9846025ecd8c05ec83a601b367c
Lehdk\Lehdk <leh99dk.mail@gmail.com>
2023-05-15 10:36:53 +0200

    Added javadoc

--- src/main/java/dk/abandonship/dataaccess/interfaces/IDatabaseLogDAO.java
+++ src/main/java/dk/abandonship/dataaccess/interfaces/IDatabaseLogDAO.java
@@ -4,6 +4,13 @@ import java.sql.SQLException;
 
 public interface IDatabaseLogDAO {
 
+    /**
+     * Inserts a log into the database
+     * @param method The method called
+     * @param request The request from the database
+     * @param result The result from the database
+     * @throws SQLException If an error occurred
+     */
     void insertToLog(String method, String request, String result) throws SQLException;
 
 }
