f8e9fbc1421aa7225934e3459ad59115c9d3f765
TheDarkSide1992 <Jenscp171@gmail.com>
2023-05-16 11:52:07 +0200

    draws images on screen

--- src/main/java/dk/abandonship/gui/controller/PopUpController/DrawingController.java
+++ src/main/java/dk/abandonship/gui/controller/PopUpController/DrawingController.java
@@ -1,13 +1,18 @@
 package dk.abandonship.gui.controller.PopUpController;
 
+import dk.abandonship.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class DrawingController implements Initializable {
@@ -15,25 +20,36 @@ public class DrawingController implements Initializable {
 
     @FXML private Canvas canvasDrawing;
     @FXML private Button btnSaveToDoc, btnClose, tbnScreen, btnSound, btnWifi, btnJunction, bntBrush,  btnColorPicker, bntEraser;
+    private Image imageMonitor, imageSpeaker, imageWifi, imageJunctionBox;
+    private Image image;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        imageMonitor = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("img/Canvas/monitor.png")));
+        imageSpeaker = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("img/Canvas/speaker.png")));
+        imageWifi = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("img/Canvas/wifi.png")));
+        imageJunctionBox = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("img/Canvas/JunktionBoxpng.png")));
+
+        image = imageMonitor;
     }
 
     public void handleSaveToDoc(ActionEvent actionEvent) {
     }
 
     public void handleSpawnScreen(ActionEvent actionEvent) {
+        image = imageMonitor;
     }
 
     public void handleSpawnSound(ActionEvent actionEvent) {
+        image = imageSpeaker;
     }
 
     public void handleSpawnWifi(ActionEvent actionEvent) {
+        image = imageWifi;
     }
 
     public void handleSpawnJunction(ActionEvent actionEvent) {
+        image = imageJunctionBox;
     }
 
     public void handleBrush(ActionEvent actionEvent) {
@@ -54,4 +70,18 @@ public class DrawingController implements Initializable {
         Stage stage = (Stage) btnClose.getScene().getWindow();
         stage.close();
     }
+
+    public void canvasClick(MouseEvent mouseEvent) {
+        System.out.println(mouseEvent.getX() + "  :  " + mouseEvent.getY());
+
+        GraphicsContext gc = canvasDrawing.getGraphicsContext2D();
+        gc.setLineWidth(5);
+
+        //gc.fillOval(mouseEvent.getX(), mouseEvent.getY(), 10,10);
+
+        gc.drawImage(image, mouseEvent.getX()-150/2, mouseEvent.getY()-150/2 ,150,150);
+    }
+
+
+
 }
